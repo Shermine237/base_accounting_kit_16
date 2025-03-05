195c261c87e83987c5f5b24c8b6538be0dcef1cf
Charlie Rostant YOSSA <Charlieyossa@gmail.com>
2025-03-05 12:06:28 +0100

    Migration to odoo 16

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -20,25 +20,13 @@
 #
 #############################################################################
 
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    customer_credit_limit = fields.Boolean(string="Customer Credit Limit")
-
-    @api.model
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        params = self.env['ir.config_parameter'].sudo()
-        customer_credit_limit = params.get_param('customer_credit_limit',
-                                                 default=False)
-        res.update(customer_credit_limit=customer_credit_limit)
-        return res
-
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        self.env['ir.config_parameter'].sudo().set_param(
-            "customer_credit_limit",
-            self.customer_credit_limit)
+    customer_credit_limit = fields.Boolean(
+        string="Customer Credit Limit",
+        config_parameter='base_accounting_kit_16.customer_credit_limit'
+    )
