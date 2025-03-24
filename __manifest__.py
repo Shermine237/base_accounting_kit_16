0b39fb315cfb10a5744cd6aa4de25b9b6e727bda
Charlie Rostant YOSSA <Charlieyossa@gmail.com>
2025-03-24 17:34:32 +0100

    Update manifest

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,7 @@
     'company': 'Cybrosys Techno Solutions',
     'maintainer': 'Cybrosys Techno Solutions',
     'website': "https://www.cybrosys.com",
-    'depends': ['base', 'web', 'account', 'account_check_printing', 'report_xlsx', 'sale'],
+    'depends': ['base', 'web', 'account', 'account_check_printing', 'report_xlsx', 'sale', 'base_account_budget_16'],
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
