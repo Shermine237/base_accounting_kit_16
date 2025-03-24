de961d700284152cc854c7f1eaa70a9a8e88747c
Charlie Rostant YOSSA <Charlieyossa@gmail.com>
2025-03-24 14:41:37 +0100

    [FIX] Le champ company_id n'existe pas

--- wizard/partner_ledger.py
+++ wizard/partner_ledger.py
@@ -25,7 +25,7 @@ from odoo import fields, models
 
 class AccountPartnerLedger(models.TransientModel):
     _name = "account.report.partner.ledger"
-    _inherit = "account.common.partner.report"
+    _inherit = ["account.common.report", "account.common.partner.report"]
     _description = "Account Partner Ledger"
 
     amount_currency = fields.Boolean("With Currency",
@@ -35,8 +35,10 @@ class AccountPartnerLedger(models.TransientModel):
 
     def _print_report(self, data):
         data = self.pre_print_report(data)
-        data['form'].update({'reconciled': self.reconciled,
-                             'amount_currency': self.amount_currency})
+        data['form'].update({
+            'reconciled': self.reconciled,
+            'amount_currency': self.amount_currency
+        })
         return self.env.ref(
             'base_accounting_kit.action_report_partnerledger').report_action(
             self, data=data)
