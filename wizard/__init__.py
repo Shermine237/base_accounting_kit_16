e932232314ab84056b17239ab4de2cf42d02b294
Charlie Rostant YOSSA <Charlieyossa@gmail.com>
2025-03-26 12:26:08 +0100

    Remove 'Profit and Loss' and 'Balance Sheet' modifications

--- wizard/__init__.py
+++ wizard/__init__.py
@@ -28,7 +28,6 @@ from . import financial_report
 from . import general_ledger
 from . import partner_ledger
 from . import tax_report
-from . import account_profit_loss
 
 # Wizards de rapports comptables
 from . import aged_partner
